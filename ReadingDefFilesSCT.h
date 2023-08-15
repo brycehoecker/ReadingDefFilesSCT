ed56ae49656c55d900ba3ae1fcde1b6eaf384efe
brycehoecker <133408086+brycehoecker@users.noreply.github.com>
2023-08-14 20:06:00 -0700

    Update ReadingDefFilesSCT.h

--- ReadingDefFilesSCT.h
+++ ReadingDefFilesSCT.h
@@ -1,1 +1,42 @@
+// Register.h
 
+#ifndef REGISTER_H
+#define REGISTER_H
+
+#include <string>
+
+class Register {
+public:
+    // Enum for AccessMode
+    enum AccessModeType {
+        MODE0 = 0, 
+        MODE1 = 1, 
+        MODE2 = 2, 
+        MODE3 = 3  
+    };
+
+    // Default constructor
+    Register();
+
+    // Parameterized constructor
+    Register(std::string Name, uint8_t RegAddr, uint8_t nBits, uint8_t startBit, 
+             uint32_t value, AccessModeType AccessMode, uint32_t lowerBound, 
+             uint32_t upperBound, float multiplier, float offset, std::string description);
+
+    // Getter and setter methods declaration can go here
+
+private:
+    std::string Name;
+    uint8_t RegAddr;
+    uint8_t nBits;
+    uint8_t startBit;
+    uint32_t value;
+    AccessModeType AccessMode;
+    uint32_t lowerBound;
+    uint32_t upperBound;
+    float multiplier;
+    float offset;
+    std::string description;
+};
+
+#endif // REGISTER_H
