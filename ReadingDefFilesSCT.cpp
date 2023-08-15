d22bd4008a3ead6e2a023b7ce4a96a68eb1c232f
brycehoecker <133408086+brycehoecker@users.noreply.github.com>
2023-08-14 20:38:08 -0700

    Update ReadingDefFilesSCT.cpp

--- ReadingDefFilesSCT.cpp
+++ ReadingDefFilesSCT.cpp
@@ -1,12 +1,12 @@
 #include "ReadingDefFilesSCT.h"
 
 // Default constructor implementation
-Register::Register() 
+ReadingDefFilesSCT::ReadingDefFilesSCT() 
     : RegAddr(0), nBits(0), startBit(0), value(0), AccessMode(MODE0), 
       lowerBound(0), upperBound(0), multiplier(1.0f), offset(0.0f), description("") {}
 
 // Parameterized constructor implementation
-Register::Register(std::string Name, uint8_t RegAddr, uint8_t nBits, uint8_t startBit, 
+ReadingDefFilesSCT::ReadingDefFilesSCT(std::string Name, uint8_t RegAddr, uint8_t nBits, uint8_t startBit, 
                    uint32_t value, AccessModeType AccessMode, uint32_t lowerBound, 
                    uint32_t upperBound, float multiplier, float offset, std::string description)
     : Name(Name), RegAddr(RegAddr), nBits(nBits), startBit(startBit), value(value), 
