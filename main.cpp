050fa3502ad8dbc195a3c17ef909c2b4cb71ce98
brycehoecker <133408086+brycehoecker@users.noreply.github.com>
2023-08-14 20:42:18 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
+#include "ReadingDefFilesSCT.h"
 
 int main() {
     // Test the class
-    Register reg("TestRegister", 0x01, 8, 0, 100, Register::MODE1, 0, 255, 1.5, 0.5, "This is a test register");
+    ReadingDefFilesSCT regRead("TestRegister", 0x01, 8, 0, 100, ReadingDefFilesSCT::MODE1, 0, 255, 1.5, 0.5, "This is a test register");
     // Further processing and testing can be done here
 
     return 0;
