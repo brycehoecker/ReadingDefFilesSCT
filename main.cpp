559803cf5162b5a03918331bf8588f5af486eb0c
brycehoecker <133408086+brycehoecker@users.noreply.github.com>
2023-08-14 20:10:25 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Test the class
+    Register reg("TestRegister", 0x01, 8, 0, 100, Register::MODE1, 0, 255, 1.5, 0.5, "This is a test register");
+    // Further processing and testing can be done here
+
+    return 0;
+}
