84dac04f0bd99afd12301811d8ef1c6839193b70
brycehoecker <133408086+brycehoecker@users.noreply.github.com>
2023-08-14 20:50:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <string>
 #include "ReadingDefFilesSCT.h"
 
 int main() {
-    // Test the class
-    ReadingDefFilesSCT regRead("TestRegister", 0x01, 8, 0, 100, ReadingDefFilesSCT::MODE1, 0, 255, 1.5, 0.5, "This is a test register");
-    // Further processing and testing can be done here
+    // Create an object of ReadingDefFilesSCT using the parameterized constructor
+    ReadingDefFilesSCT reading("TestName", 0x01, 8, 0, 100, ReadingDefFilesSCT::MODE1, 
+                               0, 255, 1.5f, 0.5f, "This is a test description");
+
+    // Assuming you have a getter method for Name in your class, you can use it like:
+    // std::cout << "Name: " << reading.getName() << std::endl;
+
+    // For this example, without getter methods, we're just printing a statement to indicate success.
+    std::cout << "ReadingDefFilesSCT object created successfully!" << std::endl;
 
     return 0;
 }
